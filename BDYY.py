d1c258aba4b29e436be2e2f063639074fe57f71f
dhanushka294 <dhanushkabolusani@gmail.com>
2026-04-08 23:09:31 +0530

    Add files via upload

---
+++ BDYY.py
@@ -0,0 +1,15 @@
+
+
+import streamlit as st
+st.title("HAPPYYYY BIRTHDAYYY SANJUUUUUUU😘😘....")
+st.image("sanjana.jpeg")
+st.text("I am very lucky to have you in my life🥹\nWe have many memories..and I am very sorryyy if any mistake was done by me🙂\nBut I never want to lose you at any cost...No matter what happens I'll always there for you🩵\nI am very sad that i am not celebrating your bdy like how you did to me,sorryyyy.....🥲for that\nBut I alwaaysss love you even though you stop loving me👀...\nI wish you also love as i love you🤌🏻,wait...not wish its an  order😝\nInka chalaa.. cheppalani undhi but you know about my memory power kadha😁,..so gurthochinappudu cheptha le...\nLOTS OF LOVEEE🫂💕\n\n                                                                                                                                                                          ~Dhanu")
+st.markdown("#📸OUR BEAUTIFUL MEMORIES:")
+c1,c2,c3=st.columns(3)
+with c1:
+    st.image("bathukamma.jpeg")
+with c2:
+    st.image("brown_green.jpeg")
+with c3:
+    st.image("pink.jpeg")
+
